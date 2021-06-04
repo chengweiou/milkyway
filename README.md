b9f47a1222244ce0c23983848eab4a371610dd25
chengweiou <chengwei.ou.dora@gmail.com>
2021-06-04 10:48:07 -0700

    clean docker-compose

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ cp src/main/resources/application.yml ~/Desktop/docker/universe/milkyway/config/
 chmod +x uat.sh
 ./uat.sh
 ```
+
+##### 单独启动容器
+```
+docker network net
+docker run --rm -it --name pgsql -p 5432:5432 --network net -v ~/Desktop/docker/pgsql/data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=123456 -d postgres
+docker run --rm -it -d --name redis -p 6379:6379 --network net -d redis
+docker run --rm -it -d --name andromeda -p 60002:8906 --network net -v ~/Desktop/docker/universe/andromeda:/proj/ -w /proj/ openjdk java -jar ser.jar
+```
--- uat.sh
+++ uat.sh
@@ -3,7 +3,10 @@ cp build/libs/milkyway-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/milkyway/ser
 #cp src/main/resources/application.yml ~/Desktop/docker/universe/milkyway/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/milkyway/config/
 cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/milkyway/config/
+cp docker-compose.yml ~/Desktop/docker/universe/milkyway/docker-compose.yml
+cd ~/Desktop/docker/universe/milkyway
 
-docker-compose down
-docker-compose up -d
+docker compose down
+docker compose rm -f
+docker compose up -d
 
