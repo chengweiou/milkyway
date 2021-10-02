7baa883a4383c9f7c367b34ca718e292f3169203
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-10-01 21:31:16 -0700

    datetime formatter

--- uat.sh
+++ uat.sh
@@ -2,7 +2,7 @@
 cp build/libs/milkyway-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/milkyway/ser.jar
 #cp src/main/resources/application.yml ~/Desktop/docker/universe/milkyway/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/milkyway/config/
-cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/milkyway/config/
+cp src/main/resources/log4j2.yml ~/Desktop/docker/universe/milkyway/config/
 cp docker-compose.yml ~/Desktop/docker/universe/milkyway/docker-compose.yml
 cd ~/Desktop/docker/universe/milkyway
 
