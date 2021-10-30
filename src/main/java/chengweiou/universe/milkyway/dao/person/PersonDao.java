8a043e5792cd607c507942593af046ecb7a60575
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-10-30 16:46:04 -0700

    update spring fix config bug, java17

--- src/main/java/chengweiou/universe/milkyway/dao/person/PersonDao.java
+++ src/main/java/chengweiou/universe/milkyway/dao/person/PersonDao.java
@@ -1,17 +1,11 @@
 package chengweiou.universe.milkyway.dao.person;
 
 
-import chengweiou.universe.milkyway.base.dao.BaseDao;
-import chengweiou.universe.milkyway.model.SearchCondition;
-import chengweiou.universe.milkyway.model.entity.person.Person;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.jdbc.SQL;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import chengweiou.universe.milkyway.base.dao.BaseDao;
+import chengweiou.universe.milkyway.model.entity.person.Person;
 
 @Repository
 @Mapper
