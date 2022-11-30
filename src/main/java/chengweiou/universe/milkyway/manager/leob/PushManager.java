4e4049e9de8522174a93a9b14583dcde57d00307
chengwei ou <chengwei.ou.dora@gmail.com>
2022-11-29 16:59:23 -0800

    async deprecate

--- src/main/java/chengweiou/universe/milkyway/manager/leob/PushManager.java
+++ src/main/java/chengweiou/universe/milkyway/manager/leob/PushManager.java
@@ -1,10 +1,9 @@
 package chengweiou.universe.milkyway.manager.leob;
 
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import chengweiou.universe.blackhole.exception.FailException;
@@ -19,10 +18,10 @@ public class PushManager {
     private PushService pushService;
 
     @Async
-    public Future<Long> pushAsync(Push e) throws FailException {
+    public CompletableFuture<Long> pushAsync(Push e) throws FailException {
         Rest<Long> rest = pushService.push(e);
-        if (rest.getCode() != BasicRestCode.OK) new AsyncResult<>(0);
-        return new AsyncResult<>(rest.getData());
+        if (rest.getCode() != BasicRestCode.OK) return CompletableFuture.completedFuture(0L);
+        return CompletableFuture.completedFuture(rest.getData());
     }
 
 }
